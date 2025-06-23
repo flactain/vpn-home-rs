4b9546e589d94ca7e857ac62ae2d674bd83f5136
moriyamato <flactly@gmail.com>
2025-06-24 02:49:24 +0900

    poc: add env file, create openidconnect. next todo: propagate state to handlers

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,3 +1,17 @@
+use openidconnect::{core::{CoreClient, CoreProviderMetadata}, reqwest, ClientId, IssuerUrl};
+
 pub async fn login() {
-    
+    let http_client = reqwest::blocking::ClientBuilder::new()
+        // Following redirects opens the client up to SSRF vulnerabilities.
+        .redirect(reqwest::redirect::Policy::none())
+        .build()
+        .expect("Cliend Cannot Build");
+
+    //configure OpenID Connect Provider
+    let provider_metadata = CoreProviderMetadata::discover(
+        &IssuerUrl::new("http://localhost:8080".to_string()).unwrap(),
+        &http_client,
+    ).unwrap();
+
+    let client = CoreClient::from_provider_metadata(provider_metadata, ClientId::new("vpn-home"), client_secret);
 }
