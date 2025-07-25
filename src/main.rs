16daa11fd61ea2f031f516f42957adf98e65d3bc
moriyamato <flactly@gmail.com>
2025-07-25 22:11:15 +0900

    pre-commit test

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,7 @@ async fn main() {
     let config = Config::from_env().unwrap();
 
     // application setting
-    let pool = PgPoolOptions::new()
-        .max_connections(20)
+    let pool = PgPoolOptions::new().max_connections(20)
         .connect("")
         .await
         .unwrap();
