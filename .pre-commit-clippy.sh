f19239b68c64544390362ab1b87186776dbaaa91
moriyamato <flactly@gmail.com>
2025-08-11 07:13:56 +0900

    chore: precommit-hook.

--- .pre-commit-clippy.sh
+++ .pre-commit-clippy.sh
@@ -2,6 +2,6 @@
 echo 'cargo fmt'
 cargo fmt
 echo 'cargo clippy(unused imports)'
-cargo clippy --fix --allow-dirty --allow-staged -- -W unused-imports
+cargo clippy --fix --allow-dirty --allow-staged
 
 git add .
